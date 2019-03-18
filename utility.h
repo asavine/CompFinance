7d3e753afeacef06f9922e8740753e6e93a9401e
ThomasNDam <48675943+ThomasNDam@users.noreply.github.com>
2019-03-18 11:54:29 +0100

    Update utility.h

--- utility.h
+++ utility.h
@@ -13,7 +13,7 @@ template<class CONT, class T, class IT = T*>
 //      has all original points
 //      plus additional ones if requested
 //      plus additional ones so maxDx is not exceeded
-//  Original continer and addPOints must be sorted
+//  Original container and addPOints must be sorted
 //  Returned container is sorted
 inline CONT
 fillData(
